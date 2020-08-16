1cca546124709cd4aedf61f5bf253ca4cb7e7d87
Ricardo Do Vale <pataco80@gmail.com>
2020-08-16 18:45:42 +0200

    add h2 from the test

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ const indexPage = () => {
   return (
     <Layout>
       <h1>IndexPage</h1>
+      <h2>IndexPage</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
     </Layout>
   )
