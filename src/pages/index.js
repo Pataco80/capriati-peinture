3f2be1d270ef9226bfb7c82befaf2f71f9c4396d
Ricardo Do Vale <pataco80@gmail.com>
2020-08-17 11:10:30 +0200

    correction error title import and use

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Tittle from '../components/Title'
+import Title from '../components/Title'
 const indexPage = () => {
   return (
     <Layout>
-      <Tittle tag='h1' title='IndexPage 1'/>
+      <Title tag='h1' title='IndexPage 1'/>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
-      <Tittle tag='h2'>IndexPage 2</Tittle>
+      <Title tag='h2'>IndexPage 2</Title>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
-      <Tittle tag='h3'>IndexPage 3</Tittle>
+      <Title tag='h3'>IndexPage 3</Title>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
-      <Tittle tag='h4'>IndexPage 4</Tittle>
+      <Title tag='h4'>IndexPage 4</Title>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
-      <Tittle tag='h5'>IndexPage 5</Tittle>
+      <Title tag='h5'>IndexPage 5</Title>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
-      <Tittle tag='h6'>IndexPage 6</Tittle>
+      <Title tag='h6'>IndexPage 6</Title>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi illo, eum dolorum nihil dolores eligendi earum nulla quae amet cumque voluptatum itaque laudantium ut deleniti repudiandae sapiente, sint consectetur eius.</p>
     </Layout>
   )
