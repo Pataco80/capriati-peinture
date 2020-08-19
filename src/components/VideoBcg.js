67855660a0c424e12dd47fe097c51e79a6e0acb8
Ricardo Do Vale <pataco80@gmail.com>
2020-08-19 12:57:00 +0200

    ajout d'autres formats vidéos et poster

--- src/components/VideoBcg.js
+++ src/components/VideoBcg.js
@@ -2,16 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 import {setLinearBcg, setFlex} from '../theme/helpers'
 
-import videoMp4 from '../assets/video/paint.mp4'
+import  { useStaticQuery, graphql } from 'gatsby'
 
+import videoMp4 from '../assets/video/paint.mp4'
+import videoOgv from '../assets/video/paint.mp4'
+import videoWebm from '../assets/video/paint.WebM'
+
+//import poster from '../assets/images/painting_poster_hero.jpg'
+
+// GraphQl Queries
+const getImage = graphql`
+  query {
+    posterImg:file(relativePath: { eq: "images/painting_poster_hero.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+// Component
 const Videobcg = ({className, children}) => {
+  const data = useStaticQuery(getImage)
+  const poster = data.posterImg.childImageSharp.fluid
+  console.log(poster)
   
   return (
     <div className={className}>
       <span className='overlay'></span>
       {children}
-      <video id='video' autoPlay loop playsInline>
+      <video id='video' autoPlay loop playsInline poster={poster}>
+        <source src={videoWebm} type="video/webm" /> 
         <source src={videoMp4} type="video/mp4" /> 
+        <source src={videoOgv} type="video/ogg" />
       </video>
     </div>
   )
