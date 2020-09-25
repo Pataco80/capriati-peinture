9da16b03475715f624b2095c25dc05d322b4eb04
Ricardo Do Vale <pataco80@gmail.com>
2020-09-25 17:36:50 +0200

    Create Parteners component

--- src/components/Parteners.js
+++ src/components/Parteners.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import {graphql, useStaticQuery} from 'gatsby'
+
+// Import components from Gatsby and plugins Gatsby
+import { graphql, useStaticQuery } from 'gatsby'
+
+// Import Components for App
 import Title from './Title'
 import PartenersItem from './PartenersItem'
+
+// Import styled-components and helpers
 import styled from 'styled-components'
-import {Section, SectionContentCenter} from './styledElements/SectionStyled'
+import { Section, SectionContentCenter } from './styledElements/SectionStyled'
 import { media, setFlex } from '../theme/helpers'
 
+// GraphQl Queries
 const getData = graphql`
   {
     logos:allFile(filter: {relativePath: {regex: "/parteners/"}}) {
@@ -21,28 +28,34 @@ const getData = graphql`
       }
     }
     partenersData: allPartnersDataJson {
-        nodes {
-          id
-          name
-          shortName
-          logo
-          altLogo
-          url
-          urlTitle
-        }
+      nodes {
+        id
+        name
+        shortName
+        logo
+        altLogo
+        url
+        urlTitle
       }
+    }
   }
 `
+
+// Component
 const Parteners = () => {
+
+  // Component Variables
   const data = useStaticQuery(getData)
-  console.log(data)
   const logoList = data.logos.edges
   const parteners = data.partenersData.nodes
 
+  // Component Functions
   const partenersLogo = logoList.map(({ node }) => ({
     logo: node.childImageSharp.fluid,
     originalName: node.childImageSharp.fluid.originalName,
   }))
+
+  // Render Component
   return (
     <PartenersWrapper>
       <Title tag='h2' title='Nos Partenaires' titleSection />
@@ -53,22 +66,25 @@ const Parteners = () => {
             const regExp = new RegExp(shortName, "i");
             const logoPath = partenersLogo.find(({originalName}) => originalName.match(regExp))
             .logo
-            return <PartenersItem 
-            Key={i} 
-            fluid={logoPath} 
-            altLogo={altLogo} 
-            url={url} 
-            urlTitle={urlTitle}
+
+            return <PartenersItem
+              Key={i}
+              fluid={logoPath}
+              altLogo={altLogo}
+              url={url}
+              urlTitle={urlTitle}
             />
           })
         }
-        
       </PartenersLogosContainer>
     </PartenersWrapper>
   )
 }
 
-const PartenersWrapper = styled(Section)``
+// Styles from styled-components
+const PartenersWrapper = styled(Section)`
+  ${setFlex({flDir:'column'})};
+`
 
 const PartenersLogosContainer = styled(SectionContentCenter)`
   ${setFlex({flDir:'column'})};
--- src/components/PartenersItem.js
+++ src/components/PartenersItem.js
@@ -1,10 +1,19 @@
 import React from 'react'
+
+// Import components from Gatsby and plugins Gatsby
 import Img from 'gatsby-image'
+
+// Import styled-components and helpers
 import styled from 'styled-components'
 import { media, setFlex } from '../theme/helpers'
 
+// Component
 const PartenersItem = (props) => {
+
+  // Component Variables
   const { fluid, altLogo, url, urlTitle } = props
+
+  // Render Component
   return (
     <PartenersItemWrapper href={url} title={urlTitle}>
       <PartenersLogo fluid={fluid} alt={altLogo}/>
@@ -12,13 +21,15 @@ const PartenersItem = (props) => {
   )
 }
 
+
+// Styles from styled-components
 const PartenersItemWrapper = styled.a`
   ${setFlex({y:'flex-start'})};
   width:100%;
   max-width:30rem;
   margin-bottom:3rem;
   text-align:center;
-  
+
   ${media.greaterThan('tablet')`
     max-width:45%;
   `}
@@ -26,11 +37,13 @@ const PartenersItemWrapper = styled.a`
   ${media.greaterThan('lgTablet')`
     max-width:30%;
   `}
+
   &:nth-child(2) {
     ${media.greaterThan('lgTablet')`
-    order:1;
-  `}
+      order:1;
+    `}
   }
+
   &:last-child {
     margin-bottom:0;
   }
@@ -39,4 +52,5 @@ const PartenersItemWrapper = styled.a`
 const PartenersLogo = styled(Img)`
   max-width:25rem;
 `
+
 export default PartenersItem
