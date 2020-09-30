8049b987308fe466f4875caf76bc39cec829e595
Ricardo Do Vale <pataco80@gmail.com>
2020-10-01 00:39:06 +0200

    Création des références

--- src/pages/works.js
+++ src/pages/works.js
@@ -7,6 +7,7 @@ import {graphql} from 'gatsby'
 import Layout from '../components/Globals/Layout'
 import SEO from '../components/Globals/SEO'
 import Hero from '../components/Hero'
+import References from '../components/References'
 
 // GraphQl Queries
 export const getImage = graphql`
@@ -29,6 +30,7 @@ const worksPage = ({data}) => {
     <Layout>
       <SEO title='Nos Travaux' description="Nos travaux éféctués au fil des années" />
       <Hero title='Nos Travaux' bcgImage={data.heroBcg.childImageSharp.fluid} />
+      <References />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, excepturi eaque corrupti ipsa ratione beatae quas tenetur architecto dolores, mollitia saepe accusantium amet recusandae dicta voluptatem explicabo illo aut a?s</p>
     </Layout>
   )
