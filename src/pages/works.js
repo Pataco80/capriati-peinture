9f0ff28e0219ff2832b73d6dbcac5a31a563a034
Ricardo Do Vale <pataco80@gmail.com>
2020-10-13 18:48:11 +0200

    retrait de Works component suite à une erreur compilation

--- src/pages/works.js
+++ src/pages/works.js
@@ -8,7 +8,7 @@ import Layout from '../components/Globals/Layout'
 import SEO from '../components/Globals/SEO'
 import Hero from '../components/Hero'
 import References from '../components/References'
-import Works from '../components/Works/Works'
+//import Works from '../components/Works/Works'
 
 // GraphQl Queries
 export const getImage = graphql`
@@ -31,8 +31,7 @@ const worksPage = ({data}) => {
     <Layout>
       <SEO title='Nos Travaux' description="Nos travaux éféctués au fil des années" />
       <Hero title='Nos Travaux' bcgImage={data.heroBcg.childImageSharp.fluid} />
-      <Works />
-      <References />
+//      <References />
     </Layout>
   )
 }
