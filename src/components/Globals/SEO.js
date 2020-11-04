92ad2c9d4891868207df8e1bb93d65d1f2e556fd
Ricardo Do Vale <pataco80@gmail.com>
2020-11-04 11:53:22 +0100

    test avec cover.png sur services

--- src/components/Globals/SEO.js
+++ src/components/Globals/SEO.js
@@ -30,7 +30,7 @@ function SEO({ description, lang, meta, title, image, keywords }) {
   const metaDescription = description || site.siteMetadata.description
 
   const url = site.siteMetadata.siteUrl
-  const metaImage = `${url}${image || "/assets/images/logos/logoCapriati.png"}`
+  const metaImage = `${url}/assets/images/logos/${image || "logoCapriati.png"}`
   const metaKeywords = keywords || site.siteMetadata.keywords
 
 
