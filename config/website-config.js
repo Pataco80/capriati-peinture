12956317c8970924a2496d44b2e70cff77ce67a6
Ricardo Do Vale <pataco80@gmail.com>
2020-11-10 00:45:28 +0100

    ajout de données et mise en forme des fichiers de configuration du site
    ajout de données de website-config.js

--- config/website-config.js
+++ config/website-config.js
@@ -1,35 +1,54 @@
 const businessInfo = {
-  name:'Capriati SA',
-  shortName:'Capriati',
+  name: 'Capriati SA',
+  shortName: 'Capriati',
   altName: 'Capriati Peinture',
-  bossInfo:{
-    firstName : 'Vincent',
+  bossInfo: {
+    firstName: 'Vincent',
     lastName: 'Capriati',
     fullName: 'Vincent Capriati', // Username to display in the author segment.
     birthday: '13 January 1985',
-    bossDescription:"Décris ton parcours depuis ta formation, tes études, bref, tout ce qui t'a conduit au poste que tu occupes.",
-    bossSlogan: "Décris la citation définissant les valeurs de ton entreprise",
+    position: 'Directuer Administratif',
+    bossDescription:
+      "Entré en tant qu'apprentis peintre au sein de l'entreprise familiale, j'ai effectué ma formation CFC et suivi par l'obtention d'une Maîtrise fédérale de Chef d'Entreprise. Depuis mars 2020, j'ai repris la succession de l'entreprise.",
+    bossSlogan: 'Notre qualité au service des couleurs de votre lieu de vie.',
   },
-  address:{
-    road:'Z.A. La Pièce',
+  address: {
+    road: 'Z.A. La Pièce',
     roadNumber: '20',
-    zipCode:'1180',
-    city:'Rolle',
-    canton:'Vaud',
-    shortCanton:'VD',
-    country:'Suisse',
-    shortCountry:'CH',
+    zipCode: '1180',
+    city: 'Rolle',
+    canton: 'Vaud',
+    shortCanton: 'VD',
+    country: 'Suisse',
+    shortCountry: 'CH',
   },
-  email: 'capriati@bluewin.ch', // Email used for RSS feed's author segment
-  userTwitter:'@CapriatiSA',
-  userFacebook:'Capriati Peinture'
+  email: 'capriati@bluewin.ch', // Email used for RSS feed"s author segment
+  userTwitter: '@CapriatiSA',
+  userFacebook: 'Capriati Peinture',
 }
 
 // dates formats functions
-const months = ["janvier", "février", "mars","avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+const months = [
+  'janvier',
+  'février',
+  'mars',
+  'avril',
+  'mai',
+  'juin',
+  'juillet',
+  'août',
+  'septembre',
+  'octobre',
+  'novembre',
+  'décembre',
+]
 let current_datetime = new Date()
-let formatted_date = current_datetime.getDate() + " " + months[current_datetime.getMonth()] + " " + current_datetime.getFullYear()
-console.log(formatted_date)
+let formatted_date =
+  current_datetime.getDate() +
+  ' ' +
+  months[current_datetime.getMonth()] +
+  ' ' +
+  current_datetime.getFullYear()
 
 const websiteConfig = {
   siteTitle: `${businessInfo.name}`, // Site title.
@@ -40,62 +59,72 @@ const websiteConfig = {
   siteLanguage: 'fr',
   siteUrl: 'https://capriati-peinture.netlify.app', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "Société de peinture en bâtiments de la famille Capriati. Entreprise familiale basée à Rolle et travaillant sur la région de 'La Côte' dans le canton de Vaud (CH)",
+  siteDescription:
+    'Société de peinture en bâtiments de la famille Capriati. Entreprise familiale basée à Rolle et travaillant sur la région de "La Côte" dans le canton de Vaud (CH)',
   // Website description used for RSS feeds/meta description tag.
   siteLanguage: 'fr', // Facebook Language
-  keywords:['Entreprise de peinture','Entreprise familiale','Peinture à Rolle','Peinture','Plâtre','Décoration','Papier-peint','Giclage'],
-  // dateFromFormat: 'DD-MMM-YYYY', // Date format used in the frontmatter.
+  keywords: [
+    'Entreprise de peinture',
+    'Entreprise familiale',
+    'Peinture à Rolle',
+    'Peinture',
+    'Plâtre',
+    'Décoration',
+    'Papier-peint',
+    'Giclage',
+  ],
+  // dateFromFormat: "DD-MMM-YYYY", // Date format used in the frontmatter.
   dateFormat: `${formatted_date}`, // Date format for display.
-  // siteRss: '/rss.xml', // Path to the RSS file.
-  // siteRssTitle: 'Gatsby Advanced Starter RSS feed', // Title of the RSS feed
-  // siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  // googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
-  // disqusShortname: 'https-vagr9k-github-io-gatsby-advanced-starter', // Disqus shortname.
+  // siteRss: "/rss.xml", // Path to the RSS file.
+  // siteRssTitle: "Gatsby Advanced Starter RSS feed", // Title of the RSS feed
+  // siteFBAppID: "1825356251115265", // FB Application ID for using app insights
+  // googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+  // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
   //postsPerPage: 4, // Amount of posts displayed per listing page.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   socialLinks: [
     {
       label: 'Facebook',
       url: 'https://facebook.com/Capriati-Peinture',
-      iconClassName: 'fa fa-facebook'
+      iconClassName: 'fa fa-facebook',
     },
     {
       label: 'Twitter',
       url: 'https://twitter.com/Vagr9K',
-      iconClassName: 'fa fa-twitter'
+      iconClassName: 'fa fa-twitter',
     },
     {
       label: 'Email',
       url: `mailto:${businessInfo.email}`,
-      iconClassName: 'fa fa-envelope'
-    }
+      iconClassName: 'fa fa-envelope',
+    },
   ],
   author: `${businessInfo.bossInfo.fullName}`,
+  authorDescription: `${businessInfo.bossInfo.bossDescription}`,
+  authorSlogan: `${businessInfo.bossInfo.bossSlogan}`,
+  authorPosition: `${businessInfo.bossInfo.position}`,
   twitterUserName: `${businessInfo.userTwitter}`,
   facebookUserName: `${businessInfo.userFacebook}`,
   copyright: `Copyright © ${new Date().getFullYear()}. Capriati S.A.`, // Copyright string for the footer of the website and RSS feed.
   themeColor: '#EE332D', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#fff' // Used for setting manifest background color.
-};
+  backgroundColor: '#fff', // Used for setting manifest background color.
+}
 
 // Validate
 
 // Make sure pathPrefix is empty if not needed
 if (websiteConfig.pathPrefix === '/') {
-  websiteConfig.pathPrefix = '';
+  websiteConfig.pathPrefix = ''
 } else {
   // Make sure pathPrefix only contains the first forward slash
-  websiteConfig.pathPrefix = `/${websiteConfig.pathPrefix.replace(/^\/|\/$/g, '')}`;
+  websiteConfig.pathPrefix = `/${websiteConfig.pathPrefix.replace(/^\/|\/$/g, '')}`
 }
 
-// Make sure siteUrl doesn't have an ending forward slash
-if (websiteConfig.siteUrl.substr(-1) === '/')
-websiteConfig.siteUrl = config.siteUrl.slice(0, -1);
+// Make sure siteUrl doesn"t have an ending forward slash
+if (websiteConfig.siteUrl.substr(-1) === '/') websiteConfig.siteUrl = config.siteUrl.slice(0, -1)
 
 // Make sure siteRss has a starting forward slash
 if (websiteConfig.siteRss && websiteConfig.siteRss[0] !== '/')
-websiteConfig.siteRss = `/${websiteConfig.siteRss}`;
+  websiteConfig.siteRss = `/${websiteConfig.siteRss}`
 
-
-
-module.exports = websiteConfig;
+module.exports = websiteConfig
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,29 +4,32 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
- // Import informations from websiteConfig configuration. For more details see the comments of the websiteConfig-config-example.js configuration file
+// Import informations from websiteConfig configuration. For more details see the comments of the websiteConfig-config-example.js configuration file
 const websiteConfig = require('./config/website-config')
 
 const pathPrefix = websiteConfig.pathPrefix
 const siteUrl = websiteConfig.siteUrl + pathPrefix
 
 module.exports = {
-/* General Information */
+  /* General Information */
   pathPrefix: websiteConfig.pathPrefix,
   siteMetadata: {
-  siteUrl: siteUrl, // For gatsby-plugin-sitemap
-  pathPrefix,
-  title: websiteConfig.siteTitle,
-  description: websiteConfig.siteDescription,
-  keywords: websiteConfig.keywords,
-  logo: websiteConfig.siteLogo,
-  siteLanguage: websiteConfig.siteLanguage,
-  author: websiteConfig.author,
-  twitterUserName: websiteConfig.twitterUserName,
-  facebookUserName: websiteConfig.facebookUserName,
-},
+    siteUrl: siteUrl, // For gatsby-plugin-sitemap
+    pathPrefix,
+    title: websiteConfig.siteTitle,
+    description: websiteConfig.siteDescription,
+    keywords: websiteConfig.keywords,
+    logo: websiteConfig.siteLogo,
+    siteLanguage: websiteConfig.siteLanguage,
+    author: websiteConfig.author,
+    authorPosition: websiteConfig.authorPosition,
+    authorDescription: websiteConfig.authorDescription,
+    authorSlogan: websiteConfig.authorSlogan,
+    twitterUserName: websiteConfig.twitterUserName,
+    facebookUserName: websiteConfig.facebookUserName,
+  },
 
-/* Your site config here */
+  /* Your site config here */
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
@@ -34,8 +37,8 @@ module.exports = {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
         host: `${siteUrl}`,
-        sitemap: `${siteUrl}/sitemap.xml`
-      }
+        sitemap: `${siteUrl}/sitemap.xml`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -68,6 +71,7 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -75,12 +79,25 @@ module.exports = {
           {
             family: `Noto Sans JP`,
             subsets: [`latin`],
-            variants: [`100`, `300`, `400`, `500`, `700`, `900`]
+            variants: [`100`, `300`, `400`, `500`, `700`, `900`],
           },
           {
             family: `Roboto`,
             subsets: [`latin`],
-            variants: [`100`, `300`, `400`, `500`, `700`, `900`, `100i`, `300i`, `400i`, `500i`, `700i`, `900i`]
+            variants: [
+              `100`,
+              `300`,
+              `400`,
+              `500`,
+              `700`,
+              `900`,
+              `100i`,
+              `300i`,
+              `400i`,
+              `500i`,
+              `700i`,
+              `900i`,
+            ],
           },
         ],
       },
@@ -100,48 +117,48 @@ module.exports = {
         icon: websiteConfig.favicon,
         icons: [
           {
-           src: "\/favicons/android-icon-36x36.png",
-           sizes: "36x36",
-           type: "image\/png",
-           density: "0.75"
+            src: '/favicons/android-icon-36x36.png',
+            sizes: '36x36',
+            type: 'image/png',
+            density: '0.75',
           },
           {
-           src: "\/favicons/android-icon-48x48.png",
-           sizes: "48x48",
-           type: "image\/png",
-           density: "1.0"
+            src: '/favicons/android-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            density: '1.0',
           },
           {
-           src: "\/favicons/android-icon-72x72.png",
-           sizes: "72x72",
-           type: "image\/png",
-           density: "1.5"
+            src: '/favicons/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            density: '1.5',
           },
           {
-           src: "\/favicons/android-icon-96x96.png",
-           sizes: "96x96",
-           type: "image\/png",
-           density: "2.0"
+            src: '/favicons/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            density: '2.0',
           },
           {
-           src: "\/favicons/android-icon-144x144.png",
-           sizes: "144x144",
-           type: "image\/png",
-           density: "3.0"
+            src: '/favicons/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            density: '3.0',
           },
           {
-           src: "\/favicons/android-icon-192x192.png",
-           sizes: "192x192",
-           type: "image\/png",
-           density: "4.0"
-          }
+            src: '/favicons/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            density: '4.0',
+          },
         ],
         icon_options: {
           purpose: `maskable`,
         },
         crossOrigin: `use-credentials`,
-        cache_busting_mode: 'none'
-      }
+        cache_busting_mode: 'none',
+      },
     },
     {
       resolve: 'gatsby-plugin-offline',
@@ -149,9 +166,9 @@ module.exports = {
         // lets you specify pages whose resources should be precached by the service worker. Ex: precachePages: [`/about_us/`, `/services/*`],
         precachePages: [`/about/`, `/services/`, `/contact/`],
         workboxConfig: {
-          globPatterns: ['**/assets/images/**/*']
-        }
-      }
+          globPatterns: ['**/assets/images/**/*'],
+        },
+      },
     },
   ],
 }
