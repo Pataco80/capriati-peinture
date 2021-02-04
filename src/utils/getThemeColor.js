c69254e70c3e996a78157fca02d8d7357f5e12d6
Ricardo Do Vale <pataco80@gmail.com>
2021-02-04 13:48:38 +0100

    déplacement de ces fichiers dans le dossier ressources

--- src/utils/getThemeColor.js
+++
@@ -1,9 +0,0 @@
-import { setTheme } from '../theme/helpers'
-const getThemeColor = () => {
-  const theme = typeof window !== 'undefined' && window.__theme
-
-  if(theme === 'dark') return `${setTheme.dark.background}`
-  if(theme === 'light') return `${setTheme.light.background}`
-}
-
-export default getThemeColor
