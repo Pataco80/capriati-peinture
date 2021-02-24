f37bc298bd0ee23b3a5a2be3d708a14b2f50e66e
Ricardo Do Vale <pataco80@gmail.com>
2021-02-24 16:53:58 +0100

    correction googleMaps

--- src/utils/getGoogleMap.js
+++ src/utils/getGoogleMap.js
@@ -1,9 +1,9 @@
 const mapsSelector = () => {
   if (
     /* if we're on iOS, open in Apple Maps */
-    navigator.platform.indexOf('iPhone') != -1 ||
-    navigator.platform.indexOf('iPod') != -1 ||
-    navigator.platform.indexOf('iPad') != -1
+    navigator.platform.indexOf('iPhone') !== -1 ||
+    navigator.platform.indexOf('iPod') !== -1 ||
+    navigator.platform.indexOf('iPad') !== -1
   )
     window.open(
       'maps://www.google.com/maps/dir//Capriati+SA,+Z.A+La+Pièce,+Rolle/@46.461034,6.2502239,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x478c45cd8d86ae71:0x17c367dba8727fac!2m2!1d6.3202635!2d46.4609371'
